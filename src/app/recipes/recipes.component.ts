4a7c516928fc14ddfd767ce3f0b13a9d0f4497a0
Tony Murwira <tony27500@gmail.com>
2017-04-20 12:11:09 +0200

    Recipe Book Project - Componets and Databinding

---
+++ src/app/recipes/recipes.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from 'app/recipes/recipe';
+
+
+
+
+@Component({
+  selector: 'rb-recipes',
+  templateUrl: './recipes.component.html'
+})
+export class RecipesComponent implements OnInit {
+  selectedRecipe: Recipe;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
